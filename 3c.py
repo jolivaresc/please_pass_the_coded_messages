3334e68f18a4026ecb560e7f0384b5fc673b66b0
José Castillo <o.castillojl@gmail.com>
2017-06-12 19:11:28 -0500

    Create 3c.py
    This code passes 4/5 tests

--- 3c.py
+++ 3c.py
@@ -1,3 +1,23 @@
+"""
+ This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+Created on Mon Jun 12 19:09:25 2017
+
+solution.py
+
+Engineering Faculty
+UNAM
+@author: jose
+
 a=[3, 1, 4, 1]
 f=[3, 1, 4, 1, 5, 9]
 c=[3, 1, 4, 1, 5, 9,1,3,4,6,1,4,3,8,2,4,6,8]
